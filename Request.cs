2588aba1d56a3ce57fa1793187b469cf56a3fba2
Michael Novitski <michaelnovister@gmail.com>
2022-01-02 19:18:31 +0200

    created Request class

---
+++ Request.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDFproject
+{
+    internal class Request :Message
+    {
+        int? classificationLVL;
+        int urgency = 1;
+
+        public Request(actionType actionType, messageType messageType, string timeSent, bool isApproved, int urgency, int? classificationLVL) : base(actionType, messageType, timeSent)
+        {
+            this.urgency = urgency;
+            this.classificationLVL = classificationLVL; 
+        }
+    }
+}
