487f20951755c723d64ebf62a02153c2cc9bd76c
Michael Novitski <michaelnovister@gmail.com>
2022-01-03 23:01:49 +0200

    deleted using commands

--- Program.cs
+++ Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace Hector
+﻿namespace Hector
 {
     public class Program
     {
         public static void Main(string[] args)
         {
 
-
-        }
+            //to do: 1.set Message class as abstract
+            //       2.seperate files
+            //       3.working with properties in C#
+            //       4.add bin and obj files to .gitignore
+            //       5.delete unecessary using commands
+            {
+            }
     }
 }
