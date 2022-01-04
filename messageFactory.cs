1741bb3aab769951a01ecf7b07a12b629dff4162
Michael Novitski <michaelnovister@gmail.com>
2022-01-04 21:31:05 +0200

    defined factory's creation types

--- messageFactory.cs
+++ messageFactory.cs
@@ -2,5 +2,13 @@
 {
     internal class messageFactory
     {
+        public Message createMessage(string action)
+        {
+            if (action == "actionType.request") { return new Request(); }
+
+            else if (action == "actionType.response") { return new Response(); }
+
+            return null;
+        }
     }
 }
